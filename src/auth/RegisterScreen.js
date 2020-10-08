7eb854ed544536c27fa38346a16c95f383c3f614
Dario Zubaray <dzubaray@gmail.com>
2020-10-08 12:56:56 -0300

    Login y register styles

--- src/auth/RegisterScreen.js
+++ src/auth/RegisterScreen.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const RegisterScreen = () => {
     return (
-        <div>
-            <h1>RegisterScreen</h1>
-        </div>
+        <>
+            <h3 className="auth__title">Register</h3>
+            <form>
+                <input type="text" className="auth__input" placeholder="Name" name="nane" autoComplete="off" />
+                <input type="text" className="auth__input" placeholder="Email" name="email" autoComplete="off" />
+                <input type="password" className="auth__input" placeholder="Password" name="password" />
+                <input type="password" className="auth__input" placeholder="Confirm Password" name="confirm-password" />
+
+                <button type="submit" className="btn btn-primary btn-block mb-5">Register</button>
+
+                <Link
+                    to="/auth/login"
+                    className="link"
+                >
+                    Already register? Login instead
+                </Link>
+            </form>
+        </>
     )
 }
